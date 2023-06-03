5fe78aa345be286839a46dd7062f60f126dcf093
raymondtju <raymond.rtju@gmail.com>
2023-06-03 21:27:04 +0700

    change rounded

--- components/wiki/heroes/hero-card.tsx
+++ components/wiki/heroes/hero-card.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Hero } from "@prisma/client";
 import { GradiantCard } from "@/components/shared/gradiant-card";
 import Image from "next/image";
@@ -10,19 +12,19 @@ const HeroCard = ({ hero }: { hero: Hero }) => {
           src={
             hero.img.split("/image/upload")[0] +
             "/image/upload/f_auto/q_3" +
-            hero.img.split("/image/upload")[1]
+            hero.img.split("/image/upload")[1] || "/nana.jpg"
           }
           alt={hero.name}
           width={96}
           height={128}
           quality={1}
-          className="absolute inset-0 w-full overflow-hidden  rounded-2xl bg-cover bg-top bg-no-repeat"
+          className="absolute inset-0 w-full overflow-hidden rounded-lg bg-cover bg-top bg-no-repeat"
           loading="lazy"
-          priority={true}
+          // priority={true}
         />
       </div>
       <div className="relative w-full">
-        <p className="absolute inset-x-0 top-[-28px] mx-1  mt-[6px] rounded-full bg-navy-900/60 p-0.5 text-center text-[10px] font-medium shadow-inner shadow-navy-500/40 backdrop-blur-md">
+        <p className="absolute inset-x-0 top-[-28px] mx-1  mt-[6px] rounded-md bg-navy-900/60 p-0.5 text-center text-[10px] font-medium shadow-inner shadow-navy-500/40 backdrop-blur-md">
           {hero.name}
         </p>
       </div>
