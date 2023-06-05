7fe60e9796a456dbb7e413e64d1d43d0a5b5a24d
Jayson Ng <jaysonng3@gmail.com>
2023-06-05 13:49:47 +0800

    adding [hero] path

--- components/wiki/heroes/heroes-container.tsx
+++ components/wiki/heroes/heroes-container.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Fragment, useEffect, useState } from "react";
-
+import { useRouter } from "next/navigation";
 import { Hero } from "@prisma/client";
 import useHeroFilter from "@/lib/state/useHeroFilter";
 import HeroesFilter from "./heroes-filter";
@@ -12,6 +12,7 @@ interface IHeroesContainer {
 }
 
 const HeroesContainer = ({ heroes }: IHeroesContainer) => {
+  const router = useRouter();
   const heroFilter = useHeroFilter();
   const [hero, setHero] = useState<Hero[]>();
 
@@ -35,37 +36,25 @@ const HeroesContainer = ({ heroes }: IHeroesContainer) => {
       <div className="grid grid-cols-3 gap-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7">
         {hero?.length === undefined
           ? heroes?.map((hero, i) => {
-                return (
-                  <div key={hero.id}>
-                    <HeroCard hero={hero} />
-                    {/* <GradiantCard className="w-fit cursor-pointer p-1.5"> */}
-                    {/* <Image
-                        src={
-                          hero.img.split("/image/upload")[0] +
-                            "/image/upload/c_fill,h_256,w_192,g_north" +
-                            hero.img.split("/image/upload")[1] || "/nana.jpg"
-                        }
-                        alt={hero.name}
-                        width={96}
-                        height={128}
-                        // className="h-[128px] w-[96px] overflow-hidden rounded-lg bg-cover bg-top bg-no-repeat"
-                        // loading="lazy"
-                      /> */}
-                    {/* </GradiantCard> */}
-                    {/* <Image
-                      src={hero.img}
-                      width={96}
-                      height={128}
-                      alt={hero.name}
-                      loading="lazy"
-                    /> */}
-                  </div>
-                );
-              
+              return (
+                <div key={hero.id}>
+                  <HeroCard
+                    hero={hero}
+                    onClick={() => {
+                      router.push(`/wiki/heroes/${hero.name.toLowerCase()}`);
+                    }}
+                  />
+                </div>
+              );
             })
           : hero?.map((hero) => (
               <Fragment key={hero.id}>
-                <HeroCard hero={hero} />
+                <HeroCard
+                  hero={hero}
+                  onClick={() => {
+                    router.push(`/wiki/heroes/${hero.name.toLowerCase()}`);
+                  }}
+                />
               </Fragment>
             ))}
       </div>
