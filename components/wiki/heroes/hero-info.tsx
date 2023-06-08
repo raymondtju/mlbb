54033e7ec969ef177bd0cd4013e8152c904b3884
Jayson Ng <jaysonng3@gmail.com>
2023-06-08 16:18:34 +0800

    gatau lg ajg

--- components/wiki/heroes/hero-info.tsx
+++ components/wiki/heroes/hero-info.tsx
@@ -1,27 +1,33 @@
 "use client";
 
 import { GradiantCard } from "@/components/shared/gradiant-card";
-import { Hero } from "@prisma/client";
+import { Hero, HeroDetails } from "@prisma/client";
 import { Progress } from "@/components/shared/progress";
 import Image from "next/image";
 
-export default function HeroFyi({ hero }: { hero: Hero | null }) {
-  const heroData = [
+export default function HeroFyi({
+  hero,
+  heroDetails,
+}: {
+  hero: Hero | null;
+  heroDetails: HeroDetails | null;
+}) {
+  const data = [
     {
       name: "Ability",
-      value: hero?.details.ability,
+      value: heroDetails?.ability,
     },
     {
       name: "Offense",
-      value: hero?.details.offense,
+      value: heroDetails?.offense,
     },
     {
       name: "Durability",
-      value: hero?.details.durability,
+      value: heroDetails?.durability,
     },
     {
       name: "Difficulty",
-      value: hero?.details.difficulty,
+      value: heroDetails?.difficulty,
     },
   ];
   return (
@@ -39,7 +45,9 @@ export default function HeroFyi({ hero }: { hero: Hero | null }) {
 
           <div className="flex flex-col gap-x-4 sm:gap-x-8">
             <div className="flex flex-row items-center gap-2">
-              <p className="font-heading text-xl md:text-3xl">{hero?.name}</p>
+              <p className="font-heading text-xl md:text-3xl">
+                {heroDetails?.heroName}
+              </p>
               <Image
                 src={`https://res.cloudinary.com/dvm5vog2j/image/upload/v1686042255/mlbb.fyi/heroRole/${hero?.role[0]}.webp`}
                 alt={hero?.role[0] || ""}
@@ -59,38 +67,38 @@ export default function HeroFyi({ hero }: { hero: Hero | null }) {
             </div>
             <div className="flex flex-row items-center">
               <Image
-                src={`https://res.cloudinary.com/dvm5vog2j/image/upload/v1685987710/mlbb.fyi/heroType/${hero?.details.heroType}.webp`}
-                alt={hero?.details.heroType || ""}
+                src={`https://res.cloudinary.com/dvm5vog2j/image/upload/v1685987710/mlbb.fyi/heroType/${heroDetails?.heroType}.webp`}
+                alt={heroDetails?.heroType || ""}
                 width={20}
                 height={20}
                 className="mr-2 h-[20px] w-[20px]"
               />
               <p className="text-semibold text-[12px] text-gray-500 sm:text-sm">
-                {`${hero?.details.heroType} (${hero?.role[0]}${
+                {`${heroDetails?.heroType} (${hero?.role[0]}${
                   hero?.role[1] ? " - " + hero?.role[1] : ""
                 })`}
               </p>
             </div>
 
-            <div className="my-2 flex flex-row gap-4 sm:my-4  sm:gap-8">
+            <div className="my-3 flex flex-row gap-4 sm:my-4  sm:gap-8">
               <div className="flex flex-col">
-                <p className="font-heading text-[10px] sm:text-[16px]">
+                <p className="font-heading text-[12px] sm:text-[16px]">
                   Winrate
                 </p>
                 <p className="text-[12px] sm:text-[20px]">{hero?.win}</p>
               </div>
               <div className="flex flex-col">
-                <p className="font-heading text-[10px] sm:text-[16px]">Pick</p>
+                <p className="font-heading text-[12px] sm:text-[16px]">Pick</p>
                 <p className="text-[12px] sm:text-[20px]">{hero?.use}</p>
               </div>
               <div className="flex flex-col">
-                <p className="font-heading text-[10px] sm:text-[16px]">Ban</p>
+                <p className="font-heading text-[12px] sm:text-[16px]">Ban</p>
                 <p className="text-[12px] sm:text-[20px]">{hero?.ban}</p>
               </div>
             </div>
 
-            {heroData.map((item, i) => (
-              <div key={i} className="mt-1 sm:mt-2">
+            {data.map((item, i) => (
+              <div key={i} className="mt-[6px] sm:mt-2">
                 <div className="flex justify-between">
                   <p className="text-[12px]">{item.name}</p>
                 </div>
