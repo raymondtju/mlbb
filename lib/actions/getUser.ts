4b4ab7c8f541faede74b22a93ea026f2d61c8368
raymondtju <raymond.rtju@gmail.com>
2023-06-11 11:43:13 +0700

    empty repo

--- lib/actions/getUser.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "@/lib/prismadb";
-
-export default async function getUser(username: string) {
-  return await prisma.user.findFirst({
-    where: {
-      username,
-    },
-  });
-}
