38187f7034d94f3bbb386a5bd0bce1b540b337fa
raymondtju <raymond.rtju@gmail.com>
2023-06-19 10:51:59 +0700

    fix/max width of bio profile

--- components/profile/profile-stats/fav.tsx
+++ components/profile/profile-stats/fav.tsx
@@ -46,7 +46,7 @@ const Favourites: React.FC<FavouritesProps> = ({
               <div className="flex items-center gap-1">
                 <Trophy className="h-4 w-4" />
                 <p className="text-[12px]">
-                  <span className="text-xl font-sat font-semibold">{item.win}</span> / {item.total}{" "}
+                  <span className="font-sat text-xl font-semibold">{item.win}</span> / {item.total}{" "}
                   matches
                   {/* {((item.win * 100) / item.total).toFixed(2)}% */}
                 </p>
