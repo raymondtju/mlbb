8e4682a0c86c3db9fe454b67525577c12e22914b
raymondtju <raymond.rtju@gmail.com>
2023-08-06 22:16:16 +0700

    tests: integrate flask

--- app/api/tier-script/route.ts
+++ app/api/tier-script/route.ts
@@ -1,12 +1,21 @@
 import { NextResponse } from "next/server";
-import { exec } from "child_process";
+import { exec, spawn } from "child_process";
 
 export async function GET(request: Request) {
   const scriptPath = `tierlist-model.py`;
+  let python = spawn('python', ['tierlist-model.py`']);
+  let dataToSend = '';
+  for await (const data of python.stdout){
+    //console.log(data.toString());
+    dataToSend += data.toString()
+  }
+  console.log("dataToSend", dataToSend);
 
   try {
-    exec(`py ${scriptPath}`, (error, stdout, stderr) => {
+    exec(`py /${scriptPath}`, (error, stdout, stderr) => {
       if (error) {
+
+        console.log(error);
         return NextResponse.json(error, { status: 500 });
       }
       return NextResponse.json(
